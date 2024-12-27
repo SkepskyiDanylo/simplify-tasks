97bbf4b79292de81cd1d47cd2491519aa53b28bf
Danylo Skepskyi <kol230305@gmail.com>
2024-12-27 18:57:14 +0100

    Development: Created django models and register them into admin panel
    Created django models Worker, Position, TaskType, Task, Team, Project, Tag
    Added Worker as a AUTH_USER_MODEL in settings
    Registered all models into django admin panel, with list_display configuration
    Added static files configuration
    Added manager/urls.py

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from .models import (
+    Position,
+    TaskType,
+    Task,
+    Team,
+    Project,
+    Worker,
+    Tag
+)
 
-# Register your models here.
+admin.site.unregister(Group)
+
+@admin.register(Worker)
+class AuthUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position", )
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "priority", "is_completed")
+    list_filter = ("is_completed", "priority", "tags")
+    search_fields = ("name",)
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "team")
--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,76 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, related_name="workers", blank=True, null=True)
+
+    class Meta:
+        ordering = ("position", "username")
+
+    def __str__(self):
+        return f"{self.username}, ({self.first_name}, {self.last_name})"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    workers = models.ManyToManyField(Worker, related_name="teams")
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="projects")
+
+    def __str__(self):
+        return f"{self.name} (Team: {self.team})"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Task(models.Model):
+    choices = [
+        (1,"Lowest"),
+        (2,"Low"),
+        (3,"Medium"),
+        (4,"High"),
+        (5,"Highest"),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.IntegerField(choices=choices, default=3)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE, related_name="tasks")
+    assigners = models.ManyToManyField(Worker, related_name="tasks")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks", blank=True, null=True)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ("deadline", "priority", "name")
+
+    def __str__(self):
+        return f"{self.name}, (Completed {self.is_completed})"
