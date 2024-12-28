80a863ed8f6839a4c52554f294f0b0ac162b4901
Danylo Skepskyi <kol230305@gmail.com>
2024-12-28 20:17:53 +0100

    Develop: Created "accounts/logout" view
    Configured accounts/looged_out.html
    Addded path "account/logout" to manager/urls.py
    Created UserLogoutView

--- manager/urls.py
+++ manager/urls.py
@@ -3,6 +3,7 @@ from manager import views
 
 urlpatterns = [
     path("accounts/login/", views.UserLoginView.as_view(), name="login"),
+    path("accounts/logout/", views.UserLogoutView.as_view(), name="logout"),
 ]
 
 app_name = "manager"
--- manager/views.py
+++ manager/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+
 from .forms import LoginForm
 
 class UserLoginView(LoginView):
   template_name = "accounts/login.html"
   form_class = LoginForm
+
+class UserLogoutView(LogoutView):
+  template_name = "accounts/logged_out.html"
