70a66f71c1db3b04c81a82c57957817ec4cad317
Danylo Skepskyi <kol230305@gmail.com>
2024-12-29 17:25:27 +0100

    Develop: Added WorkerDetailView
    Addded WorkerDetailView
    Added path "profile/<int:pk>/" to manager/urls.py
    Created accounts/profile.html template

--- manager/urls.py
+++ manager/urls.py
@@ -4,6 +4,7 @@ from manager import views
 urlpatterns = [
     path("accounts/login/", views.UserLoginView.as_view(), name="login"),
     path("accounts/logout/", views.UserLogoutView.as_view(), name="logout"),
+    path("profile/<int:pk>/", views.WorkerDetailView.as_view(), name="profile"),
 ]
 
 app_name = "manager"
--- manager/views.py
+++ manager/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import DetailView
 
 from .forms import LoginForm
+from .models import Worker
+
 
 class UserLoginView(LoginView):
   template_name = "accounts/login.html"
@@ -9,3 +12,13 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
   template_name = "accounts/logged_out.html"
+
+class WorkerDetailView(DetailView):
+  model = Worker
+  context_object_name = "user"
+  template_name = "accounts/profile.html"
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context["segment"] = "profile"
+    return context
