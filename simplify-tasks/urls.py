f66f7cc7b27dc1555236b6203c592cb9679ae7e1
Danylo Skepskyi <kol230305@gmail.com>
2025-04-08 17:09:49 +0200

    Dev: loaded staticfiles dir

--- simplify-tasks/urls.py
+++ simplify-tasks/urls.py
@@ -25,12 +25,11 @@ urlpatterns = [
     path("", include("manager.urls", namespace="manager"), name="manager"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT
-    )
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
