7688b02c80eb7e2eb0c039377239bec18a1c6d78
Danylo Skepskyi <kol230305@gmail.com>
2025-04-09 16:48:54 +0200

    Dev: moved local static settings to dev

--- simplify-tasks/settings/dev.py
+++ simplify-tasks/settings/dev.py
@@ -10,3 +10,13 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
